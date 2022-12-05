7f5b57c39c61953c2e2f229a6a79ff4e4240a207
intsuc <intsuc@gmail.com>
2022-12-06 02:21:44 +0900

    Add agent

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,44 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.22"
+  kotlin("jvm") version "1.7.22"
+  id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
+version = "0.1.0"
+
 repositories {
-    mavenCentral()
+  mavenCentral()
+  maven("https://libraries.minecraft.net")
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-scripting-common:1.7.22")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:1.7.22")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host:1.7.22")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:1.7.22")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven:1.7.22")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    testImplementation(kotlin("test"))
+  implementation("org.jetbrains.kotlin:kotlin-scripting-common:1.7.22")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:1.7.22")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host:1.7.22")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:1.7.22")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven:1.7.22")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+  compileOnly("com.mojang:brigadier:1.0.18")
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+  kotlinOptions.jvmTarget = "17"
+}
+
+tasks.jar {
+  manifest {
+    attributes(
+      "Premain-Class" to "mckts.Agent",
+    )
+  }
+}
+
+tasks.shadowJar {
+  archiveVersion.set("")
 }
---
+++ src/main/kotlin/mckts/Agent.kt
@@ -0,0 +1,57 @@
+package mckts
+
+import java.lang.instrument.ClassFileTransformer
+import java.lang.instrument.Instrumentation
+import java.nio.file.Paths
+import java.security.ProtectionDomain
+import java.util.jar.JarFile
+import kotlin.io.path.createDirectories
+import kotlin.io.path.outputStream
+
+@Suppress("unused")
+object Agent {
+  @JvmStatic
+  fun premain(
+    args: String?,
+    instrumentation: Instrumentation,
+  ) {
+    instrumentation.appendToSystemClassLoaderSearch(getOrUnpackBrigadier())
+    instrumentation.addTransformer(object : ClassFileTransformer {
+      override fun transform(
+        loader: ClassLoader?,
+        className: String,
+        classBeingRedefined: Class<*>?,
+        protectionDomain: ProtectionDomain?,
+        classfileBuffer: ByteArray,
+      ): ByteArray? {
+        return null // TODO
+      }
+    })
+  }
+
+  private fun getOrUnpackBrigadier(): JarFile {
+    return JarFile("server.jar").use { server ->
+      server
+        .getInputStream(server.getEntry("META-INF/libraries.list"))
+        .bufferedReader()
+        .use { libraries ->
+          val brigadierPath = libraries
+            .readLines()
+            .map { it.split('\t')[2] }
+            .first { it.startsWith("com/mojang/brigadier/") }
+          server
+            .getInputStream(server.getEntry("META-INF/libraries/$brigadierPath"))
+            .buffered()
+            .use { brigadier ->
+              val outputPath = Paths.get("libraries", brigadierPath)
+              outputPath.parent.createDirectories()
+              outputPath
+                .outputStream()
+                .buffered()
+                .use { output -> brigadier.transferTo(output) }
+              JarFile(outputPath.toFile())
+            }
+        }
+    }
+  }
+}
