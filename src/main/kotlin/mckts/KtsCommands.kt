003294c8e30aad968fca66386eb7850ef729722b
intsuc <intsuc@gmail.com>
2022-12-06 09:13:10 +0900

    Support direct eval

--- src/main/kotlin/mckts/KtsCommands.kt
+++ src/main/kotlin/mckts/KtsCommands.kt
@@ -4,6 +4,7 @@ import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.arguments.StringArgumentType.greedyString
 import java.io.File
 import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.api.SourceCode
 import kotlin.script.experimental.api.implicitReceivers
 import kotlin.script.experimental.host.toScriptSource
 import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
@@ -17,18 +18,46 @@ object KtsCommands {
     dispatcher: CommandDispatcher<Any>,
   ) {
     dispatcher.register(
-      literal("kts:eval").then(
-        argument("file", greedyString()).executes {
-          eval(dispatcher, it.source, it["file"])
-        }
-      )
+      literal("kts:eval")
+        .then(
+          literal("code").then(
+            argument("code", greedyString())
+              .executes {
+                evalCode(dispatcher, it.source, it["code"])
+              }
+          )
+        )
+        .then(
+          literal("file").then(
+            argument("file", greedyString())
+              .executes {
+                evalFile(dispatcher, it.source, it["file"])
+              }
+          )
+        )
     )
   }
 
-  private fun eval(
+  private fun evalCode(
+    dispatcher: CommandDispatcher<Any>,
+    source: Any,
+    code: String,
+  ): Int {
+    return evalSourceCode(dispatcher, source, code.toScriptSource())
+  }
+
+  private fun evalFile(
     dispatcher: CommandDispatcher<Any>,
     source: Any,
     file: String,
+  ): Int {
+    return evalSourceCode(dispatcher, source, File(file).toScriptSource())
+  }
+
+  private fun evalSourceCode(
+    dispatcher: CommandDispatcher<Any>,
+    source: Any,
+    code: SourceCode,
   ): Int {
     val compilationConfiguration = createJvmCompilationConfigurationFromTemplate<McScript> {
       implicitReceivers(CommandExecutor::class)
@@ -37,7 +66,7 @@ object KtsCommands {
       implicitReceivers(CommandExecutor(dispatcher, source))
     }
 
-    val result = BasicJvmScriptingHost().eval(File(file).toScriptSource(), compilationConfiguration, evaluationConfiguration)
+    val result = BasicJvmScriptingHost().eval(code, compilationConfiguration, evaluationConfiguration)
 
     result.reports.forEach {
       if (it.severity > ScriptDiagnostic.Severity.WARNING) {
